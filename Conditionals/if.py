5f079a36597c9070ed69f4c33596e9ae952e438e
SylwiaKolodziej1 <106033716+SylwiaKolodziej1@users.noreply.github.com>
2022-06-10 19:26:00 +0200

    Add files via upload

---
+++ Conditionals/if.py
@@ -0,0 +1,12 @@
+print("Give first number: ")
+a = int(input())
+
+print("Give second number: ")
+b = int(input())
+
+if (a > b):
+    print("First number is bigger than the second one")
+elif (a < b):
+    print("First number is smaller than the second one")
+else:
+    print("Numbers are equal")
