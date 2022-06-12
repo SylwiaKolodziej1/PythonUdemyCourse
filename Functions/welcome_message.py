243c00d06d56859d4457eb945211cfd2088417e5
SylwiaKolodziej1 <106033716+SylwiaKolodziej1@users.noreply.github.com>
2022-06-12 11:05:11 +0200

    Add files via upload

---
+++ Functions/welcome_message.py
@@ -0,0 +1,15 @@
+def welcome_message(name):
+    print("Hi", name, ", nice to see you!")
+
+name = input("Give your name: ")
+
+welcome_message(name)
+
+
+
+#If we want to welcome specific people:
+
+#names = ["Sylwia", "Gosia", "Piotr", "Arek"]
+
+#for name in names:
+#   welcome_message(name)
