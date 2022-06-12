e975ce63c7271c778e83e94ef9aed6ba4846375e
SylwiaKolodziej1 <106033716+SylwiaKolodziej1@users.noreply.github.com>
2022-06-12 11:36:15 +0200

    Add files via upload

---
+++ Functions/return.py
@@ -0,0 +1,23 @@
+def rectangle_field(a, b):
+        return a * b
+
+a = int(input("Give length of the first side: "))
+b = int(input("Give length of the second side: "))
+
+print(rectangle_field(a, b))
+
+
+#2nd program
+
+
+def division(a, b):
+    if (b == 0):
+        return
+    return a / b
+
+a = int(input("Give first nuber: "))
+b = int(input("Give second nuber: "))
+
+print(division(a, b))
+        
+
